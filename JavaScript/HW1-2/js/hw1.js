8081334d0797c2cb17dc3dde4c1f56ec739354c1
Musatov Dima <lebwaua@gmail.com>
2016-03-31 21:23:33 +0300

    JS HW1-2

---
+++ JavaScript/HW1-2/js/hw1.js
@@ -0,0 +1,15 @@
+function pow(a, b) {
+    var res;
+        res = Math.pow(a, b);
+            return res;
+}
+var a=prompt('Число=');
+    if(Math.round(a)==a){
+        var b= prompt('Степень=');
+            if(Math.round(b) != b)
+                alert=("Not integer");
+            }
+    }else{
+        alert("not int");
+    }
+console.log(pow(a, b));
